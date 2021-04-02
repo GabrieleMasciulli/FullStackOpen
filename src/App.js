2e2ca2d557785fcd614a8474bb9b3134ae21b56f
Gabriele Masciulli <gmasciu@gmail.com>
2021-04-02 10:31:30 +0200

    Fetching data from server

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonsForm';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  console.log('persons', persons);
+
+  const hook = () => {
+    console.log('effect');
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log('request fulfilled');
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
