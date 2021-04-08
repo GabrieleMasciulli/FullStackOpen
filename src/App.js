c1ed80465a22c257566b42f22a11a5565bbb88fc
Gabriele Masciulli <gmasciu@gmail.com>
2021-04-08 14:51:08 +0200

    Added proxy
    All the requests which do not fetch either css or JavaScript are being redirected to the server at localhost:3001 instead of being handled in development mode (localhost:3000)

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState('some error happened...');
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -54,7 +54,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
