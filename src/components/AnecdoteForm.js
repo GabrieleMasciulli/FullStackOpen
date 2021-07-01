a1c8598508c6b0b0d0b1684695b439025aebc1e0
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-01 11:39:13 +0200

    Updated anecdote reducer, now adding new anecdotes and updating votes to the state, separated the form from the anecdotes into their own components

---
+++ src/components/AnecdoteForm.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
+const AnecdoteForm = props => {
+  const dispatcher = useDispatch()
+
+  const addAnecdote = e => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    dispatcher(createAnecdote(content))
+  }
+  return (
+    <div>
+      <h2>Create new</h2>
+
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input type='text' name='anecdote' />
+        </div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
