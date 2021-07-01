9cde6bce3b6969822425ca83afdf3b67fc25528b
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-01 16:59:32 +0200

    Sorting anecdotes by votes

--- src/components/Anecdotes.js
+++ src/components/Anecdotes.js
@@ -5,12 +5,13 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 const Anecdotes = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector(state => state)
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
 
   return (
     <div>
       <h2>Anecdotes</h2>
 
-      {anecdotes.map(anecdote => (
+      {sortedAnecdotes.map(anecdote => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
