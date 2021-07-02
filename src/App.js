a1d5dd20e9d94f299f679f3216d8c1fc809b9f44
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-02 14:25:34 +0200

    Anecdotes are now stored in the backend usign json-server as an example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Notification from './components/Notification'
 import Anecdotes from './components/Anecdotes'
 import AnecdoteForm from './components/AnecdoteForm'
 
+import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+
+import anecdoteService from './services/anecdote.service'
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <Notification />
